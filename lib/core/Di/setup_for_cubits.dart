44dceb849708026510720de8a9420d5614d1f539
shady steha <126605393+shadymohamed532001@users.noreply.github.com>
2024-05-26 18:38:14 +0300

    Add setup classes for Cubits, DataSources, and Repositories
    Added new files `setup_for_cubits.dart`, `setup_for_datasources.dart`, and `setup_for_repos.dart` to set up dependencies for Cubits, DataSources, and Repositories respectively. These files contain commented out code to register the necessary dependencies using `serviceLocator`.

---
+++ lib/core/Di/setup_for_cubits.dart
@@ -0,0 +1,18 @@
+
+class SetupForCubits {
+  void setUpForCubits() {
+    // serviceLocator.registerFactory<OnbordingCubit>(
+    //   () =>
+    //       OnbordingCubit(onBoardingRepo: serviceLocator.get<OnBoardingRepo>()),
+    // );
+    // serviceLocator.registerFactory<LoginCubit>(
+    //     () => LoginCubit(loginRepo: serviceLocator.get<LoginRepo>()));
+    // serviceLocator.registerFactory<SignUpCubit>(
+    //     () => SignUpCubit(signUpRepo: serviceLocator.get<SignUpRepo>()));
+    // serviceLocator.registerFactory<LayoutCubit>(
+    //     () => LayoutCubit(layOutRepo: serviceLocator.get<LayOutRepo>()));
+    // serviceLocator.registerFactory<HomeCubit>(
+    //     () => HomeCubit(homeRepo: serviceLocator.get<HomeRepo>()));
+  
+  }
+}
